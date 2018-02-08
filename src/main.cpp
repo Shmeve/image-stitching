d45dcf34eb6e418ab4ccba95e1ec553ec566f844
Steve Ferreira <sjferreira516@gmail.com>
2018-02-08 01:26:39 -0500

    Better corner detection

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 using namespace cv;
 using namespace std;
 
-const int SIZE = 11;
+const int SIZE = 7;
 const int MID_POINT = SIZE/2;
 
 Mat harrisCornerDetector(Mat input);
@@ -12,12 +12,14 @@ Mat nonMaximaSuppression(Mat input);
 
 int main() {
     //Mat image = imread("images/graf/img1.ppm", IMREAD_UNCHANGED);
-    Mat image = imread("images/checkers.png", IMREAD_COLOR);
-    Mat gray;
-    Mat output;
+    Mat image = imread("images/yosemite/Yosemite2.jpg", IMREAD_COLOR);
+    //Mat image = imread("images/checkers.png", IMREAD_COLOR);
+    Mat gray = Mat::zeros(image.size(), CV_32F);
+    Mat output = Mat::zeros(image.size(), CV_32F);;
 
+    imshow("Original", image);
+    image.convertTo(image, CV_32F);
     cvtColor(image, gray, COLOR_BGR2GRAY);
-    gray.convertTo(gray, CV_32F);
 
     //output.convertTo(output, CV_8U);
     //imshow("Gray 32", output);
@@ -47,17 +49,18 @@ Mat harrisCornerDetector(Mat input) {
     Mat C = Mat(input.size(), CV_32F);          // C = det(H)/Trace(H)
     Mat result = Mat(input.size(), CV_32F);     // Final result with non-maximum suppression
 
-    // Setup Sobel filters to calculate the derivatives
+    // Sobel Kernel
     float sobelFilter[] = {
-            1, 0, -1,
-            2, 0, -2,
-            1, 0, -1
+            1.0/8, 0, -1.0/8,
+            2.0/8, 0, -2.0/8,
+            1.0/8, 0, -1.0/8
     };
 
+    // Sobel Filters
     Mat s_v = Mat(3, 3, CV_32F, sobelFilter);
     Mat s_h = s_v.t();      // Transpose of horizontal Sobel matrix
 
-    // Compute Ix, Iy, Ixy
+    // Compute Ix, Iy, Ixy derivatives using Sobel
     filter2D(input, Ix, -1, s_h);
     filter2D(input, Iy, -1, s_v);
     multiply(Ix, Iy, Ixy);
@@ -68,26 +71,49 @@ Mat harrisCornerDetector(Mat input) {
     GaussianBlur(Iy, Iy, Size(3, 3), 1);
     GaussianBlur(Ixy, Ixy, Size(3, 3), 1);
 
-    // Create Harris
+    // Create Harris matrix
     pow(Ix, 2, Ix); // Ix Ix
     pow(Iy, 2, Iy); // Iy Iy
 
-    for (int i = 0; i < input.rows; i++) {
-        for (int j = 0; j < input.cols; j++) {
-            float a = Ix.at<float>(i, j);
-            float b = Ixy.at<float>(i, j);
-            float c = Ixy.at<float>(i, j);
-            float d = Iy.at<float>(i, j);
+    // Compute sum of products of the derivatives
+    Mat IxSum;
+    Mat IySum;
+    Mat IxySum;
+    float sumKernel[] = {
+            1.0/9, 1.0/9, 1.0/9,
+            1.0/9, 1.0/9, 1.0/9,
+            1.0/9, 1.0/9, 1.0/9
+    };
 
-            C.at<float>(i, j) = ((a * d) - (b * c))/(a + d);
+    Mat sumOfProducts = Mat(3, 3, CV_32F, sumKernel);
+
+    filter2D(Ix, IxSum, -1, sumOfProducts);
+    filter2D(Iy, IySum, -1, sumOfProducts);
+    filter2D(Ixy, IxySum, -1, sumOfProducts);
+
+    // Calculate corner strength matrix
+    for (int i = MID_POINT; i < input.rows - MID_POINT; i++) {
+        for (int j = MID_POINT; j < input.cols - MID_POINT; j++) {
+            float a = IxSum.at<float>(i, j);
+            float b = IxySum.at<float>(i, j);
+            float c = IxySum.at<float>(i, j);
+            float d = IySum.at<float>(i, j);
+
+            // c(H) = det/trace, det = ad-bc, trace = a+d
+            float cornerStrength = (a*d - b*c)/(a + d);
+
+            // Apply threshold
+            if (cornerStrength > 10) {
+                C.at<float>(i, j) = cornerStrength;
+            }
         }
     }
 
     // Display/Test
-    imshow("Ix", Ix);
+    /*imshow("Ix", Ix);
     imshow("Iy", Iy);
     imshow("Ixy", Ixy);
-    imshow("C", C);
+    imshow("C", C);*/
 
     waitKey(0);
 
